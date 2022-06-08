49eb298dc2ef8395a1b1d5f8a4334d1995507c39
milosdj13 <milosdjurica13@gmail.com>
2022-06-08 15:08:02 +0200

    day2, js4

--- javascript/1. Two Sum.js
+++ javascript/1. Two Sum.js
@@ -4,7 +4,7 @@
 
 // You can return the answer in any order.
 
- 
+
 
 // Example 1:
 
@@ -19,7 +19,7 @@
 
 // Input: nums = [3,3], target = 6
 // Output: [0,1]
- 
+
 
 // Constraints:
 
@@ -27,19 +27,19 @@
 // -109 <= nums[i] <= 109
 // -109 <= target <= 109
 // Only one valid answer exists.
- 
+
 
 // Follow-up: Can you come up with an algorithm that is less than O(n2) time complexity?
 
-/**
+/*
  * @param {number[]} nums
  * @param {number} target
  * @return {number[]}
  */
- var twoSum = function(nums, target) {
-    for(let i=0; i<nums.length; i++){
-        for(let j=i+1; j<nums.length; j++){
-            if(nums[i] + nums[j] == target){
+var twoSum = function (nums, target) {
+    for (let i = 0; i < nums.length; i++) {
+        for (let j = i + 1; j < nums.length; j++) {
+            if (nums[i] + nums[j] == target) {
                 return [i, j]
             }
         }
--- javascript/88. Merge Sorted Array.js
+++ javascript/88. Merge Sorted Array.js
@@ -4,7 +4,7 @@
 
 // The final sorted array should not be returned by the function, but instead be stored inside the array nums1. To accommodate this, nums1 has a length of m + n, where the first m elements denote the elements that should be merged, and the last n elements are set to 0 and should be ignored. nums2 has a length of n.
 
- 
+
 
 // Example 1:
 
@@ -25,7 +25,7 @@
 // Explanation: The arrays we are merging are [] and [1].
 // The result of the merge is [1].
 // Note that because m = 0, there are no elements in nums1. The 0 is only there to ensure the merge result can fit in nums1.
- 
+
 
 // Constraints:
 
@@ -34,24 +34,24 @@
 // 0 <= m, n <= 200
 // 1 <= m + n <= 200
 // -109 <= nums1[i], nums2[j] <= 109
- 
+
 
 // Follow up: Can you come up with an algorithm that runs in O(m + n) time?
 
 
 
-/**
+/*
  * @param {number[]} nums1
  * @param {number} m
  * @param {number[]} nums2
  * @param {number} n
  * @return {void} Do not return anything, modify nums1 in-place instead.
  */
- var merge = function(nums1, m, nums2, n) {
-    for(let i=0; i< n; i++){
-        nums1[i+m] = nums2[i]
+var merge = function (nums1, m, nums2, n) {
+    for (let i = 0; i < n; i++) {
+        nums1[i + m] = nums2[i]
     }
-    nums1 = nums1.sort( (a, b) => a-b)
+    nums1 = nums1.sort((a, b) => a - b)
 };
 
 
